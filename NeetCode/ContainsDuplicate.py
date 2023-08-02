4e62344573d1b24d8a6ec76840ed2d41e31b08ad
dimka <dmitryellison@gmail.com>
2023-08-03 00:58:22 +0300

    Initial commit

---
+++ NeetCode/ContainsDuplicate.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def containsDuplicate(self, nums) -> bool:
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+
+        nums.sort()
+
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i-1]:
+                return True
+
+        return False
+
