4f697b2542a506dbe8f56c492f1aa896f71bae81
dimka <dmitryellison@gmail.com>
2023-08-06 15:07:25 +0300

    LongestCommonPrefix.py o(nk) where k = len(min(word_in_list))

--- LeetCode150/LongestCommonPrefix.py
+++ LeetCode150/LongestCommonPrefix.py
@@ -4,19 +4,11 @@ from typing import List
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         prefix = strs[0]
-
         for word in strs[1:]:
-            for i in range(len(word)+1):
-                if i == len(word):
-                    prefix = prefix[:i]
-
-                if i+1 > len(prefix):
-                    break
-
-                if prefix[i] != word[i]:
-                    prefix = prefix[:i]
-
-
+            i=0
+            while i < len(prefix) and i < len(word) and prefix[i] == word[i]:
+                i += 1
+            prefix = prefix[:i]
         return prefix
 
 
