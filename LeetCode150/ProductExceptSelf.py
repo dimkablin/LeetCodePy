e968d28cb16cb2db215f5da0b08f116c413fd264
dimka <dmitryellison@gmail.com>
2023-08-07 00:44:03 +0300

    ProductExceptSelf.py o(2n)

---
+++ LeetCode150/ProductExceptSelf.py
@@ -0,0 +1,14 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        result = [1]*n
+
+        for i in range(1, len(nums)):
+            result[i] = result[i-1]*nums[i-1]
+
+        temp = 1
+        for i in range(n-1, -1, -1):
+            result[i] *= temp
+            temp *= nums[i]
+
+        return result
