27cb00dc0633440ee4ebf6400791e07425df1229
dimka <dmitryellison@gmail.com>
2023-08-20 14:39:36 +0300

    ValidPalindrome.py o(n)

--- LeetCode150/ValidPalindrome.py
+++ LeetCode150/ValidPalindrome.py
@@ -4,15 +4,15 @@ class Solution:
         j = len(s)-1
         s = s.lower()
 
-        while( i != j):
-            if not s[i].isalpha():
+        while i < j:
+            if not s[i].isalpha() and not s[i].isdigit():
                 i += 1
                 continue
-            if not s[j].isalpha():
+            if not s[j].isalpha() and not s[j].isdigit():
                 j -= 1
+                continue
 
             if s[i] != s[j]:
-                print(i, s[i], j, s[j])
                 return False
 
             i += 1
@@ -22,8 +22,7 @@ class Solution:
 
 
 if __name__ == "__main__":
-    print(int("a"), int("z"))
 
     solution = Solution()
-    res = solution.isPalindrome("A man, a plan, a canal: Panama")
+    res = solution.isPalindrome("0P")
     print(res)
